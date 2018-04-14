13b96425b5331ffe589c75f2e8cae83031405eca
Gwen <no.thanks@example.com>
2018-04-14 17:49:25 +0200

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,31 @@
+WhatYear
+--------
+
+A small script written using only native modules in python2/3 that returns a matching
+list of years that such a date can exist on. It's built with heavy input cleaning, so
+it will allow you to use the minimum non-ambiguous strings to represent the date.
+
+For example:
+
+./whatyear.py friday 13 september
+For the day Friday, 13 September:
+Matching years: [2013, 2002, 1996, 1991, 1985, 1974, 1968, 1963, 1957, 1946] 
+
+./whatyear.py thurs 13 a
+Month ambiguous. Could be ['April', 'August'] 
+
+./whatyear.py t 25 jul
+Weekday ambiguous. Could be ['Tuesday', 'Thursday'] 
+
+./whatyear.py tu 31 jun
+Month day not in range: [1,30]
+
+./whatyear.py thurs 13 april
+For the day Thursday, 13 April:
+Matching years: [2017, 2006, 2000, 1995, 1989, 1978, 1972, 1967, 1961, 1950] 
+
+./whatyear.py F 29 Feb
+For the day Friday, 29 February:
+Matching years: [2008, 1980, 1952, 1924, 1884, 1856, 1828, 1788, 1760, 1732]
+
+
---
+++ whatyear.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+import argparse, datetime, calendar
+parser = argparse.ArgumentParser()
+parser.add_argument("day_of_week")
+parser.add_argument("day_of_month")
+parser.add_argument("month")
+args = parser.parse_args()
+
+MAX_RESULTS = 10
+
+REFERENCE_YEAR=1600 # Must be in the past _and_ a leap year
+
+weekdays=list(calendar.day_name)
+months=list(calendar.month_name)
+
+def name_selector(option, olist):
+    '''Generically select the best matching text option from a list of texts.
+Give a different error if you don't match any compared to matching too many.'''
+    answers=set()
+    for e in olist:
+        for l in range(len(e),0,-1):
+            short_e=e[:l]
+            if short_e.lower() == option.lower():
+                answers.add(e)
+                break
+    if len(answers)==0:
+        raise ValueError('Option not found in list.')
+    if len(answers)>1:
+        raise IndexError('Option ambiguous. Could be %s ' % str(list(answers)))
+    return list(answers)[0]
+
+try:
+    day_of_week=name_selector(args.day_of_week,weekdays)
+except ValueError as e:
+    print(str(e).replace('Option','Weekday'))
+    exit(1)
+except IndexError as e:
+    print(str(e).replace('Option','Weekday'))
+    exit(1)
+dow = weekdays.index(day_of_week)
+
+try:
+    month=name_selector(args.month,months)
+except ValueError as e:
+    print(str(e).replace('Option','Month'))
+    exit(1)
+except IndexError as e:
+    print(str(e).replace('Option','Month'))
+    exit(1)
+m = months.index(month)
+
+max_dom=calendar.monthrange(REFERENCE_YEAR,m)[1]
+try:
+    day_of_month=int(args.day_of_month)
+    if (day_of_month < 1) or (day_of_month > max_dom):
+        raise ValueError
+except ValueError:
+    print('Month day not in range: [1,%s]'% max_dom)
+    exit(1)
+dom = day_of_month
+
+matchingyears=[]
+thisyear = datetime.date.today().year
+for year in range(thisyear,REFERENCE_YEAR,-1):
+    try:
+        testdate = datetime.date(year,m,dom)
+    except ValueError: #Only on 29th Feb
+        continue
+    if testdate.weekday() == dow:
+        matchingyears.append(year)
+    if len(matchingyears) >= MAX_RESULTS: break
+
+print('For the day %s, %s %s:' % (day_of_week, day_of_month, month))
+print('Matching years: %s ' % str(matchingyears))
